0560239569d8d14df08d8f0dab23ab6e0fcdacd1
Matheus M. Parrela <matheusparrela13@gmail.com>
2021-12-02 12:05:13 -0300

    Atualização

--- integracao/integracao.c
+++ integracao/integracao.c
@@ -4,20 +4,42 @@
 #include <locale.h>
 
 double met_trapezio(int m, double x_0, double x_n);    
-double met_simpson_1(int m, double x_0, double x_n);    //intervalos pares
-double met_simpson_2(int m, double x_0, double x_n);    //intervalos multiplos de 3
+double met_simpison_1(int m, double x_0, double x_n);    //intervalos pares
+double met_simpison_2(int m, double x_0, double x_n);    //intervalos multiplos de 3
 double funcao(double x);
+int cria_vetor();
+double preenche_vetor(double *vetor, int n);
 
 
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
-    printf("\nMétodos de Integração Numérica\n\n");
+    int menu, n;
 
-    printf("\nTrapézio - I = %.6lf\n",met_trapezio(6, 4, 5.2));
-    printf("\nSimpson 1/3 - I = %.6lf\n",met_simpson_1(6, 4, 5.2));
-    printf("\nSimpson 3/8 - I = %.6lf\n",met_simpson_2(6, 4, 5.2));
+    printf("Menu:\n");
+    printf("1 - Dado poontos de uma função\n2 - Dada uma função\n0 - Sair\n");
+      scanf("%d", &menu);
+
+    while(menu != 0){  
+      if(menu == 1){
+        
+         n = cria_vetor();
+            double vetor[n]; 
+        preenche_vetor(vetor, n);
+
+        menu = 0;
+      }
+
+      if(menu == 2){
+        printf("\nMétodos de Integração Numérica\n\n");
+
+        printf("\nTrapézio - I = %.6lf\n",met_trapezio(6, 4, 5.2));
+        printf("\nSimpson 1/3 - I = %.6lf\n",met_simpison_1(6, 4, 5.2));
+        printf("\nSimpson 3/8 - I = %.6lf\n",met_simpison_2(6, 4, 5.2));
+        menu = 0;
+      }
+    }
 
     return 0;
 }
@@ -30,6 +52,36 @@ double funcao(double x){
     resultado = log(x); //Funçao dada - Escreva sua função
 
   return resultado;
+}
+
+int cria_vetor(){
+
+  int pontos = 0;
+
+  printf("Insira o número de ponto: ");
+      scanf("%d", &pontos);
+  
+  return pontos;
+}
+
+
+double preenche_vetor(double *vetor, int n){
+
+
+  printf("Preenchendo os Pontos Y:\n");
+
+  int i = 0;
+
+  for(i = 0; i < n; i++){
+    printf("Y[%d] =",i);
+      scanf("%lf", &vetor[i]);
+  }
+
+  for(i = 0; i < n; i++){
+    printf("Y[%d] = %.5lf\n", i, vetor[i]);
+  }
+
+  return 0;
 }
 
 
@@ -51,7 +103,7 @@ double met_trapezio(int m, double x_0, double x_n){
 }
 
 
-double met_simpson_1(int m, double x_0, double x_n){ 
+double met_simpison_1(int m, double x_0, double x_n){ 
 
     double h = 0, resultado = 0 , x = 0;
     int i = 0;
@@ -69,7 +121,7 @@ double met_simpson_1(int m, double x_0, double x_n){
 }
 
 
-double met_simpson_2(int m, double x_0, double x_n){ 
+double met_simpison_2(int m, double x_0, double x_n){ 
 
     double h = 0, resultado = 0 , x = 0;
     int i = 0;
