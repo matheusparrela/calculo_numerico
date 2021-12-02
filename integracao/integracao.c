20497de20313fec6f625ef59d4864874035b2348
Matheus M. Parrela <matheusparrela13@gmail.com>
2021-12-02 09:59:19 -0300

    Atualização

--- integracao/integracao.c
+++ integracao/integracao.c
@@ -4,8 +4,8 @@
 #include <locale.h>
 
 double met_trapezio(int m, double x_0, double x_n);    
-double met_simpison_1(int m, double x_0, double x_n);    //intervalos pares
-double met_simpison_2(int m, double x_0, double x_n);    //intervalos multiplos de 3
+double met_simpson_1(int m, double x_0, double x_n);    //intervalos pares
+double met_simpson_2(int m, double x_0, double x_n);    //intervalos multiplos de 3
 double funcao(double x);
 
 
@@ -16,8 +16,8 @@ int main(){
     printf("\nMétodos de Integração Numérica\n\n");
 
     printf("\nTrapézio - I = %.6lf\n",met_trapezio(6, 4, 5.2));
-    printf("\nSimpson 1/3 - I = %.6lf\n",met_simpison_1(6, 4, 5.2));
-    printf("\nSimpson 3/8 - I = %.6lf\n",met_simpison_2(6, 4, 5.2));
+    printf("\nSimpson 1/3 - I = %.6lf\n",met_simpson_1(6, 4, 5.2));
+    printf("\nSimpson 3/8 - I = %.6lf\n",met_simpson_2(6, 4, 5.2));
 
     return 0;
 }
@@ -51,7 +51,7 @@ double met_trapezio(int m, double x_0, double x_n){
 }
 
 
-double met_simpison_1(int m, double x_0, double x_n){ 
+double met_simpson_1(int m, double x_0, double x_n){ 
 
     double h = 0, resultado = 0 , x = 0;
     int i = 0;
@@ -69,7 +69,7 @@ double met_simpison_1(int m, double x_0, double x_n){
 }
 
 
-double met_simpison_2(int m, double x_0, double x_n){ 
+double met_simpson_2(int m, double x_0, double x_n){ 
 
     double h = 0, resultado = 0 , x = 0;
     int i = 0;
